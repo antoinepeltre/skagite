422170b4a5ea970fb745f660d4a79d4fe6f624ac
antoinepeltre <antoine.peltre@gmail.com>
2024-11-26 12:33:47 +0100

    [EDIT] Strucuture home + reservation

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { ReservationComponent } from "../reservation/reservation.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [HeaderComponent, ReservationComponent]
+    imports: [HeaderComponent, ReservationComponent, FooterComponent]
 })
 export class HomeComponent {
 
--- src/app/components/reservation/reservation.component.ts
+++ src/app/components/reservation/reservation.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 @Component({
   selector: 'app-reservation',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, MatDatepickerModule, MatNativeDateModule, MatFormFieldModule],
   templateUrl: './reservation.component.html',
   styleUrl: './reservation.component.scss'
 })
