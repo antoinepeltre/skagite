49096c0f54504d9c240e5eb21eae32977a215d7b
antoinepeltre <antoine.peltre@gmail.com>
2024-12-02 18:22:31 +0100

    [EDIT] package.json to inject environment variables before build]

---
+++ environment-variables.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const dotenv = require('dotenv');
+
+// Charger les variables d'environnement à partir du fichier .env
+dotenv.config();
+
+// Spécifie où se trouve ton fichier environment.prod.ts
+const environmentFilePath = path.join(__dirname, 'src', 'environments', 'environment.prod.ts');
+
+// Lis le contenu du fichier environment.prod.ts
+let environmentFileContent = fs.readFileSync(environmentFilePath, 'utf-8');
+
+// Remplace les placeholders dans environment.prod.ts par les valeurs des variables d'environnement
+environmentFileContent = environmentFileContent.replace(
+  'supabaseUrl: \'\',',
+  `supabaseUrl: '${process.env.SUPABASE_URL}',`
+);
+
+environmentFileContent = environmentFileContent.replace(
+  'supabaseKey: \'\',',
+  `supabaseKey: '${process.env.SUPABASE_KEY}',`
+);
+
+// Sauvegarde les modifications dans le fichier environment.prod.ts
+fs.writeFileSync(environmentFilePath, environmentFileContent);
+
+console.log('Les variables d\'environnement ont été injectées avec succès.');
