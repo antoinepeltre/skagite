51031c9f0ad029815cf5498a1a69f99a0b81edb7
antoinepeltre <antoine.peltre@gmail.com>
2024-12-02 17:37:19 +0100

    Ignore src/environments folder

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    supabaseUrl: 'https://wiqskewijnpnhmgweaax.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndpcXNrZXdpam5wbmhtZ3dlYWF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI2MjA4NzYsImV4cCI6MjA0ODE5Njg3Nn0.t5BQrePjTVLFkdVzSfHPRvsiIfjR49w1Hrrq4Y4SN_0',
-};
