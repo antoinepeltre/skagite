fc8e67e0ca46e662e14933f7116b45e8f549424e
antoinepeltre <antoine.peltre@gmail.com>
2024-12-03 12:13:13 +0100

    [EDIT] Config vercel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from './components/home/home.component';
 import { RoomDetailComponent } from './components/room-detail/room-detail.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent, pathMatch: 'full' },
-    { path: '', redirectTo: '', pathMatch: 'full' },
-    {path: 'rooms/:id', component: RoomDetailComponent }
+    { path: '', component: HomeComponent, pathMatch: 'full' },
+    { path: 'rooms/:id', component: RoomDetailComponent },
+    { path: '**', redirectTo: '' } 
 ];
