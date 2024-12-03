c3f63346b0f651b2c540a32e8926f2f3a6f8a747
antoinepeltre <antoine.peltre@gmail.com>
2024-12-03 10:26:46 +0100

    [EDIT] environment-variables script

--- environment-variables.js
+++ environment-variables.js
@@ -1,28 +1,54 @@
+// const fs = require('fs');
+// const path = require('path');
+// const dotenv = require('dotenv');
+
+// // Charger les variables d'environnement à partir du fichier .env
+// dotenv.config();
+
+// // Spécifie où se trouve ton fichier environment.prod.ts
+// const environmentFilePath = path.join(__dirname, 'src', 'environments', 'environment.ts');
+
+// // Lis le contenu du fichier environment.prod.ts
+// let environmentFileContent = fs.readFileSync(environmentFilePath, 'utf-8');
+
+// // Remplace les placeholders dans environment.prod.ts par les valeurs des variables d'environnement
+// environmentFileContent = environmentFileContent.replace(
+//   'supabaseUrl: \'\',',
+//   `supabaseUrl: '${process.env.SUPABASE_URL}',`
+// );
+
+// environmentFileContent = environmentFileContent.replace(
+//   'supabaseKey: \'\',',
+//   `supabaseKey: '${process.env.SUPABASE_KEY}',`
+// );
+
+// // Sauvegarde les modifications dans le fichier environment.prod.ts
+// fs.writeFileSync(environmentFilePath, environmentFileContent);
+
+// console.log('Les variables d\'environnement ont été injectées avec succès.');
+
 const fs = require('fs');
 const path = require('path');
-const dotenv = require('dotenv');
 
-// Charger les variables d'environnement à partir du fichier .env
-dotenv.config();
+console.log('SUPABASE_URL:', process.env.SUPABASE_URL);
+console.log('SUPABASE_KEY:', process.env.SUPABASE_KEY);
 
-// Spécifie où se trouve ton fichier environment.prod.ts
-const environmentFilePath = path.join(__dirname, 'src', 'environments', 'environment.ts');
+const environment = `
+export const environment = {
+  production: true,
+  supabaseUrl: '${process.env.SUPABASE_URL}',
+  supabaseKey: '${process.env.SUPABASE_KEY}'
+};
+`;
 
-// Lis le contenu du fichier environment.prod.ts
-let environmentFileContent = fs.readFileSync(environmentFilePath, 'utf-8');
+console.log('Target Path:', targetPath);
 
-// Remplace les placeholders dans environment.prod.ts par les valeurs des variables d'environnement
-environmentFileContent = environmentFileContent.replace(
-  'supabaseUrl: \'\',',
-  `supabaseUrl: '${process.env.SUPABASE_URL}',`
-);
+const targetPath = path.join(__dirname, 'src/environments/environment.prod.ts');
 
-environmentFileContent = environmentFileContent.replace(
-  'supabaseKey: \'\',',
-  `supabaseKey: '${process.env.SUPABASE_KEY}',`
-);
-
-// Sauvegarde les modifications dans le fichier environment.prod.ts
-fs.writeFileSync(environmentFilePath, environmentFileContent);
-
-console.log('Les variables d\'environnement ont été injectées avec succès.');
+fs.writeFileSync(targetPath, environment, (err) => {
+  if (err) {
+    console.error('Error writing environment file', err);
+  } else {
+    console.log('Environment file generated');
+  }
+});
