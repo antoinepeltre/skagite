6c616267444387f1b424ce25065847b76f530cc9
antoinepeltre <antoine.peltre@gmail.com>
2024-12-03 13:30:48 +0100

    [EDIT] Mynode.js

--- mynode.js
+++ mynode.js
@@ -2,19 +2,31 @@ const fs = require('fs');
 const path = require('path');
 const successColor = '\x1b[32m%s\x1b[0m';
 const checkSign = '\u{2705}';
-const dotenv = require('dotenv').config({path: 'src/.env'}); ;
+const dotenv = require('dotenv').config({ path: path.resolve(__dirname, 'src/.env') });
+
+// Vérifie que les variables sont bien chargées
+console.log('SUPABASE_URL:', process.env.SUPABASE_URL);
+console.log('SUPABASE_KEY:', process.env.SUPABASE_KEY);
+
+if (!process.env.SUPABASE_URL || !process.env.SUPABASE_KEY) {
+  console.error("Les variables d'environnement SUPABASE_URL et SUPABASE_KEY sont manquantes.");
+  process.exit(1); // Arrêter si les variables sont manquantes
+}
 
 const envFile = `export const environment = {
-    SUPABASE_KEY: '${process.env.SUPABASE_KEY}',
+    production: true,
     SUPABASE_URL: '${process.env.SUPABASE_URL}',
+    SUPABASE_KEY: '${process.env.SUPABASE_KEY}',
 };
 `;
-const targetPath = path.join(__dirname, 'src/environments/environment.development.ts');
+
+const targetPath = path.join(__dirname, 'src/environments/environment.prod.ts');
+
 fs.writeFile(targetPath, envFile, (err) => {
     if (err) {
         console.error(err);
         throw err;
     } else {
-        console.log(successColor, `${checkSign} Successfully generated environment.development.ts`);
+        console.log(successColor, `${checkSign} Successfully generated environment.prod.ts`);
     }
 });
