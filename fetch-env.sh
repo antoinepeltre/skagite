291e8628b6a1538e5f740d1240f2138e5d5937a5
antoinepeltre <antoine.peltre@gmail.com>
2024-12-07 12:08:42 +0100

    [EDIT] Fetch-env

--- fetch-env.sh
+++ fetch-env.sh
@@ -5,19 +5,27 @@ ENV_URL="https://gist.githubusercontent.com/antoinepeltre/d8a0ac8f86e226183681dd
 
 # Récupérer le contenu du Gist et l'afficher avant de l'écrire dans .env
 echo "Fetching environment variables from Gist..."
-curl -s $ENV_URL
+ENV_VARS=$(curl -s $ENV_URL)
 
 # Vérifiez si le fichier .env existe déjà
 if [ -f .env ]; then
   echo ".env file already exists."
 else
-  # Récupérez le fichier brut et enregistrez-le en tant que .env
-  echo "Saving environment variables to .env..."
-  curl -s $ENV_URL -o .env
+  # Si le fichier .env n'existe pas, le créer et injecter les variables
+  echo "Creating .env file and saving environment variables..."
 
-  # Vérifiez si le fichier .env a été correctement téléchargé
+  # Vérifiez si le contenu a bien été récupéré
+  if [ -z "$ENV_VARS" ]; then
+    echo "Failed to fetch environment variables from Gist."
+    exit 1
+  fi
+
+  # Écrire le contenu récupéré dans le fichier .env
+  echo "$ENV_VARS" > .env
+
+  # Vérifier si le fichier .env a bien été créé
   if [ ! -f .env ]; then
-    echo "Failed to fetch environment variables."
+    echo "Failed to create .env file."
     exit 1
   else
     echo ".env file created successfully."
